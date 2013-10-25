58c19dd2a68ffe1174769eafd7c98891cabfd61f
Matt DeMers <matt.s.demers@gmail.com>
2013-10-25 13:29:16 -0700

    completed the course
    my completion of the course

--- matt-d-udacity-web-app/blog.py
+++ matt-d-udacity-web-app/blog.py
@@ -5,18 +5,22 @@ import hashlib
 import hmac
 import logging
 import json
+import time
+from datetime import datetime, timedelta
 from string import letters
 
 import webapp2
 import jinja2
+import logging
 
+from google.appengine.api import memcache
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
 
-secret = 'fart'
+secret = 'hangnail'
 
 def render_str(template, **params):
     t = jinja_env.get_template(template)
@@ -147,26 +151,84 @@ class Post(db.Model):
              'last_modified': self.last_modified.strftime(time_fmt)}
         return d
 
+def set_timed_cache_entry(key, entry):
+    """Save an entry to the cache and save the time of caching.
+    Returns the time the cache entry was made in datetime format"""
+    current_time = datetime.utcnow()
+    memcache.set(key,{"value":entry, "cache_time":current_time})
+    return current_time
 
+def get_timed_cache_entry(key):
+    """Retrieve a cache entry as well as the time is was cached.
+    Returns the entry and a time in datetime format.  If the entry does
+    not exist, return None."""
+    entry = memcache.get(key)
+    if entry:
+        return entry['value'], entry['cache_time']
+    else:
+        return None, None
+
+def store_and_cache_post(post):
+    post.put()
+    cache_key = post.key().id()
+    return set_timed_cache_entry(cache_key,post)
+
+def get_cached_post_and_age(post_id):
+    key = str(post_id)
+    post, cache_time = get_timed_cache_entry(key)
+    if post is None:
+        logging.error("DB QUERY")
+        post = Post.get_by_id(int(post_id), parent=blog_key())
+        if not post:
+            logging.error("Didn't find the post.")
+            return None, None
+        cache_time = set_timed_cache_entry(key,post)
+
+    cache_age = datetime.utcnow() - cache_time
+    return post, cache_age
+
+def front_posts_and_age(update=False):
+    key = 'front_posts'
+    posts, cache_time = get_timed_cache_entry(key)
+    if posts is None or update:
+        logging.error("DB QUERY")
+        posts = Post.all().order('-created')
+        posts = list(posts)
+        cache_time = set_timed_cache_entry(key,posts)
+    
+    cache_age = datetime.utcnow() - cache_time
+    return posts, cache_age
+
+def time_since_query():
+    current_time = datetime.utcnow()
+    query_time = memcache.get('cache_time')
+    if query_time:
+        return current_time - query_time
+    else:
+        return None
 
 class BlogFront(BlogHandler):
     def get(self):
-        posts = Post.all().order('-created')
+        posts, cache_age = front_posts_and_age()
+
         if self.format == 'html':
-            self.render('front.html', posts = posts)
+            self.render('front.html', posts = posts, 
+                cache_age = cache_age.seconds)
         else:
             return self.render_json([p.as_dict() for p in posts])
 
 class PostPage(BlogHandler):
     def get(self, post_id):
-        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
-        post = db.get(key)
+        #key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        #post = db.get(key)
+        post, cache_age = get_cached_post_and_age(post_id)
 
         if not post:
             self.error(404)
             return
         if self.format == 'html':
-            self.render("permalink.html", post = post)
+            self.render("permalink.html", post = post, 
+                cache_age=cache_age.seconds)
         else:
             self.render_json(post.as_dict())
 
@@ -175,7 +237,7 @@ class NewPost(BlogHandler):
         if self.user:
             self.render("newpost.html")
         else:
-            self.redirect("/login")
+            self.redirect("/blog/login")
 
     def post(self):
         if not self.user:
@@ -187,6 +249,7 @@ class NewPost(BlogHandler):
         if subject and content:
             p = Post(parent = blog_key(), subject = subject, content = content)
             p.put()
+            front_posts_and_age(update=True)
             self.redirect('/blog/%s' % str(p.key().id()))
         else:
             error = "subject and content, please!"
@@ -296,15 +359,21 @@ class Unit2Welcome(BlogHandler):
         else:
             self.redirect('/unit2/signup')
 
+class FlushCache(BlogHandler):
+    def get(self):
+        memcache.flush_all()
+        self.redirect("/blog")
+
 app = webapp2.WSGIApplication([('/', MainPage),
                                ('/unit2/signup', Unit2Signup),
                                ('/unit22/welcome', Unit2Welcome),
                                ('/blog/?(?:.json)?', BlogFront),
                                ('/blog/([0-9]+)(?:.json)?', PostPage),
-                               ('/blog/newpost', NewPost),
-                               ('/blog/signup', Register),
-                               ('/blog/login', Login),
-                               ('/blog/logout', Logout),
-                               ('/blog/welcome', Welcome),
+                               ('/blog/newpost/?', NewPost),
+                               ('/blog/signup/?', Register),
+                               ('/blog/login/?', Login),
+                               ('/blog/logout/?', Logout),
+                               ('/blog/welcome/?', Welcome),
+                               ('/blog/flush/?', FlushCache)
                                ],
                               debug=True)
